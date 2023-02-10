7b08868dbdfd870bd7aeee0d5e54174d6183c10c
Dave Raring <d@7pi.co>
2023-02-10 11:05:51 -0600

    trapping errors on outputs

--- outputs.tf
+++ outputs.tf
@@ -14,19 +14,19 @@ output "user_unique_id" {
 }
 
 output "access_key_id" {
-  value       = join("", local.access_key.*.id)
+  value       = try(join("", local.access_key.*.id), "")
   description = "The access key ID"
 }
 
 output "secret_access_key" {
   sensitive   = true
-  value       = join("", local.access_key.*.secret)
+  value       = try(join("", local.access_key.*.secret), "")
   description = "The secret access key. This will be written to the state file in plain-text"
 }
 
 output "ses_smtp_password_v4" {
   sensitive   = true
-  value       = join("", compact(local.access_key.*.ses_smtp_password_v4))
+  value       = try(join("", compact(local.access_key.*.ses_smtp_password_v4)), "")
   description = "The secret access key converted into an SES SMTP password by applying AWS's Sigv4 conversion algorithm"
 }
 
