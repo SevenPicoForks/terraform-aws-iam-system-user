01210ac288d1f62c8851543c9fd0f9128ce3d18a
Dave Raring <d@7pi.co>
2023-02-10 10:31:10 -0600

    upping context

--- main.tf
+++ main.tf
@@ -7,16 +7,16 @@ locals {
 
 # Defines a user that should be able to write to you test bucket
 resource "aws_iam_user" "default" {
-  count                = module.this.enabled ? 1 : 0
-  name                 = module.this.id
+  count                = module.context.enabled ? 1 : 0
+  name                 = module.context.id
   path                 = var.path
   force_destroy        = var.force_destroy
-  tags                 = module.this.tags
+  tags                 = module.context.tags
   permissions_boundary = var.permissions_boundary
 }
 
 resource "aws_iam_user_group_membership" "default" {
-  count      = module.this.enabled && length(var.groups) > 0 ? 1 : 0
+  count      = module.context.enabled && length(var.groups) > 0 ? 1 : 0
   user       = aws_iam_user.default[count.index].name
   groups     = var.groups
   depends_on = [aws_iam_user.default]
@@ -24,12 +24,12 @@ resource "aws_iam_user_group_membership" "default" {
 
 # Generate API credentials
 resource "aws_iam_access_key" "default" {
-  count = module.this.enabled && local.create_regular_access_key ? 1 : 0
+  count = module.context.enabled && local.create_regular_access_key ? 1 : 0
   user  = local.username
 }
 
 resource "awsutils_expiring_iam_access_key" "default" {
-  count   = module.this.enabled && local.create_expiring_access_key ? 1 : 0
+  count   = module.context.enabled && local.create_expiring_access_key ? 1 : 0
   user    = local.username
   max_age = var.iam_access_key_max_age
 }
@@ -48,18 +48,18 @@ locals {
 
 resource "aws_iam_user_policy" "inline_policies" {
   #bridgecrew:skip=BC_AWS_IAM_16:Skipping `Ensure IAM policies are attached only to groups or roles` check because this module intentionally attaches IAM policy directly to a user.
-  for_each = module.this.enabled ? local.inline_policies_map : {}
+  for_each = module.context.enabled ? local.inline_policies_map : {}
   lifecycle {
     create_before_destroy = true
   }
-  name_prefix = module.this.id
+  name_prefix = module.context.id
   user        = local.username
   policy      = each.value
 }
 
 resource "aws_iam_user_policy_attachment" "policies" {
   #bridgecrew:skip=BC_AWS_IAM_16:Skipping `Ensure IAM policies are attached only to groups or roles` check because this module intentionally attaches IAM policy directly to a user.
-  for_each = module.this.enabled ? local.policy_arns_map : {}
+  for_each = module.context.enabled ? local.policy_arns_map : {}
   lifecycle {
     create_before_destroy = true
   }
@@ -71,7 +71,7 @@ module "store_write" {
   source  = "registry.terraform.io/cloudposse/ssm-parameter-store/aws"
   version = "0.9.1"
 
-  count = module.this.enabled && var.ssm_enabled && var.create_iam_access_key ? 1 : 0
+  count = module.context.enabled && var.ssm_enabled && var.create_iam_access_key ? 1 : 0
 
   parameter_write = [
     {
@@ -90,16 +90,16 @@ module "store_write" {
     }
   ]
 
-  context = module.this.context
+  context = module.context.legacy
 }
 
 
 locals {
-  encrypted_credentials = <<EOF
+  encrypted_credentials               = <<EOF
 AWS_ACCESS_KEY_ID=${join("", local.access_key.*.id)}
 AWS_SECRET_ACCESS_KEY=${join("", local.access_key.*.secret)}
 EOF
-  pgp_key_is_keybase               = length(regexall("keybase:", var.pgp_key)) > 0 ? true : false
+  pgp_key_is_keybase                  = length(regexall("keybase:", var.pgp_key)) > 0 ? true : false
   keybase_credentials_pgp_message     = local.pgp_key_is_keybase ? templatefile("${path.module}/templates/keybase_credentials_pgp_message.txt", { encrypted_credentials = local.encrypted_credentials }) : ""
   keybase_credentials_decrypt_command = local.pgp_key_is_keybase ? templatefile("${path.module}/templates/keybase_credentials_decrypt_command.sh", { encrypted_credentials = local.encrypted_credentials }) : ""
 }
